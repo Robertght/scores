582112bf7794f8f71f96ee1280ca1de3fffabc2f
Radu <radu.constantin@pixelgrade.com>
2017-08-29 15:17:33 +0300

    Author page improvements

--- functions.php
+++ functions.php
@@ -5,14 +5,25 @@ add_action( 'show_user_profile', 'extra_user_profile_fields' );
 add_action( 'edit_user_profile', 'extra_user_profile_fields' );
 
 function extra_user_profile_fields( $user ) { ?>
-	<h3><?php _e("Gaming info", "blank"); ?></h3>
+	<h3><?php _e( "Gaming info", "blank" ); ?></h3>
 
 	<table class="form-table">
 		<tr>
-			<th><label for="experience"><?php _e("XP"); ?></label></th>
+			<th><label for="experience"><?php _e( "XP" ); ?></label></th>
 			<td>
-				<input type="text" name="experience" id="experience" value="<?php echo esc_attr( get_the_author_meta( 'experience', $user->ID ) ); ?>" class="regular-text" readonly/><br />
-				<span class="description"><?php _e("This is all that you need to know."); ?></span>
+				<input type="text" name="experience" id="experience"
+				       value="<?php echo esc_attr( get_the_author_meta( 'experience', $user->ID ) ); ?>"
+				       class="regular-text" readonly/><br/>
+				<span class="description"><?php _e( "This is all that you need to know." ); ?></span>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="badges"><?php _e( "Badges" ); ?></label></th>
+			<td>
+				<input type="text" name="badges" id="badges"
+				       value="<?php echo esc_attr( get_the_author_meta( 'badges', $user->ID ) ); ?>"
+				       class="regular-text" /><br/>
+				<span class="description"><?php _e( "These are all the badges the user has." ); ?></span>
 			</td>
 		</tr>
 	</table>
@@ -22,10 +33,11 @@ add_action( 'personal_options_update', 'save_extra_user_profile_fields' );
 add_action( 'edit_user_profile_update', 'save_extra_user_profile_fields' );
 
 function save_extra_user_profile_fields( $user_id ) {
-	if ( !current_user_can( 'edit_user', $user_id ) ) {
+	if ( ! current_user_can( 'edit_user', $user_id ) ) {
 		return false;
 	}
 	update_user_meta( $user_id, 'experience', $_POST['experience'] );
+	update_user_meta( $user_id, 'badges', $_POST['badges'] );
 }
 
 function matched_register_post_type() {
@@ -34,18 +46,35 @@ function matched_register_post_type() {
 			'labels'      => array(
 				'name'          => __( 'Matches' ),
 				'singular_name' => __( 'Match' ),
-				'view_item' => __( 'View user page'),
+				'view_item'     => __( 'View user page' ),
 			),
 			'public'      => true,
 			'has_archive' => true,
-			'menu_icon' => 'dashicons-groups',
+			'menu_icon'   => 'dashicons-groups',
 			'description' => 'This is where everything begins',
-			'supports' => array( 'title' ),
+			'supports'    => array( 'title' ),
 		)
 	);
 }
 
+function badges_register_post_type() {
+	register_post_type( 'badges',
+		array(
+			'labels'      => array(
+				'name'          => __( 'Badges' ),
+				'singular_name' => __( 'Badge' ),
+				'view_item'     => __( 'View badge' ),
+			),
+			'public'      => true,
+			'description' => 'Badges for players',
+			'menu_icon'   => 'dashicons-tickets-alt',
+			'supports' => array( 'title', 'editor', 'thumbnail' ),
+		) );
+}
+add_theme_support('post-thumbnails');
+
 add_action( 'init', 'matched_register_post_type' );
+add_action( 'init', 'badges_register_post_type' );
 
 add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
 /**
@@ -60,12 +89,12 @@ function cmb2_sample_metaboxes() {
 	 * Initiate the metabox
 	 */
 	$cmb = new_cmb2_box( array(
-		'id'            => 'test_metabox',
-		'title'         => __( 'Test Metabox', 'cmb2' ),
-		'object_types'  => array( 'matches', ), // Post type
-		'context'       => 'normal',
-		'priority'      => 'high',
-		'show_names'    => true, // Show field names on the left
+		'id'           => 'test_metabox',
+		'title'        => __( 'Test Metabox', 'cmb2' ),
+		'object_types' => array( 'matches', ), // Post type
+		'context'      => 'normal',
+		'priority'     => 'high',
+		'show_names'   => true, // Show field names on the left
 		// 'cmb_styles' => false, // false to disable the CMB stylesheet
 		// 'closed'     => true, // Keep the metabox closed by default
 	) );
