6958172f1e70e1d9605298c36e226c6b77794379
Radu <radu.constantin@pixelgrade.com>
2017-08-29 13:23:50 +0300

    Author page + field

---
+++ functions.php
@@ -0,0 +1,107 @@
+<?php
+
+
+add_action( 'show_user_profile', 'extra_user_profile_fields' );
+add_action( 'edit_user_profile', 'extra_user_profile_fields' );
+
+function extra_user_profile_fields( $user ) { ?>
+	<h3><?php _e("Gaming info", "blank"); ?></h3>
+
+	<table class="form-table">
+		<tr>
+			<th><label for="experience"><?php _e("XP"); ?></label></th>
+			<td>
+				<input type="text" name="experience" id="experience" value="<?php echo esc_attr( get_the_author_meta( 'experience', $user->ID ) ); ?>" class="regular-text" readonly/><br />
+				<span class="description"><?php _e("This is all that you need to know."); ?></span>
+			</td>
+		</tr>
+	</table>
+<?php }
+
+add_action( 'personal_options_update', 'save_extra_user_profile_fields' );
+add_action( 'edit_user_profile_update', 'save_extra_user_profile_fields' );
+
+function save_extra_user_profile_fields( $user_id ) {
+	if ( !current_user_can( 'edit_user', $user_id ) ) {
+		return false;
+	}
+	update_user_meta( $user_id, 'experience', $_POST['experience'] );
+}
+
+function matched_register_post_type() {
+	register_post_type( 'matches',
+		array(
+			'labels'      => array(
+				'name'          => __( 'Matches' ),
+				'singular_name' => __( 'Match' ),
+				'view_item' => __( 'View user page'),
+			),
+			'public'      => true,
+			'has_archive' => true,
+			'menu_icon' => 'dashicons-groups',
+			'description' => 'This is where everything begins',
+			'supports' => array( 'title' ),
+		)
+	);
+}
+
+add_action( 'init', 'matched_register_post_type' );
+
+add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes' );
+/**
+ * Define the metabox and field configurations.
+ */
+function cmb2_sample_metaboxes() {
+
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = '_yourprefix_';
+
+	/**
+	 * Initiate the metabox
+	 */
+	$cmb = new_cmb2_box( array(
+		'id'            => 'test_metabox',
+		'title'         => __( 'Test Metabox', 'cmb2' ),
+		'object_types'  => array( 'matches', ), // Post type
+		'context'       => 'normal',
+		'priority'      => 'high',
+		'show_names'    => true, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // Keep the metabox closed by default
+	) );
+
+	// Regular text field
+	$cmb->add_field( array(
+		'name'       => __( 'Test Text', 'cmb2' ),
+		'desc'       => __( 'field description (optional)', 'cmb2' ),
+		'id'         => $prefix . 'text',
+		'type'       => 'text',
+		'show_on_cb' => 'cmb2_hide_if_no_cats', // function should return a bool value
+		// 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
+		// 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
+		// 'on_front'        => false, // Optionally designate a field to wp-admin only
+		// 'repeatable'      => true,
+	) );
+
+	// URL text field
+	$cmb->add_field( array(
+		'name' => __( 'Website URL', 'cmb2' ),
+		'desc' => __( 'field description (optional)', 'cmb2' ),
+		'id'   => $prefix . 'url',
+		'type' => 'text_url',
+		// 'protocols' => array('http', 'https', 'ftp', 'ftps', 'mailto', 'news', 'irc', 'gopher', 'nntp', 'feed', 'telnet'), // Array of allowed protocols
+		// 'repeatable' => true,
+	) );
+
+	// Email text field
+	$cmb->add_field( array(
+		'name' => __( 'Test Text Email', 'cmb2' ),
+		'desc' => __( 'field description (optional)', 'cmb2' ),
+		'id'   => $prefix . 'email',
+		'type' => 'text_email',
+		// 'repeatable' => true,
+	) );
+
+	// Add other metaboxes as needed
+
+}
