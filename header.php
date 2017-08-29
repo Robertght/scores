0f1e589a50b749a8c391b54f40c507b66d18c7de
Robert <robert.ghetau@pixelgrade.com>
2017-08-29 23:16:23 +0300

    fix colors & paddings

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo wp_get_document_title(); ?></title>
 
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
     <?php wp_head(); ?>
@@ -25,8 +24,5 @@
 <div id="page">
 
     <div id="header" role="banner">
-        <div id="headerimg">
-            <h1><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
-            <div class="description"><?php bloginfo('description'); ?></div>
-        </div>
+            <h1 class="site-title"><a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
     </div>
