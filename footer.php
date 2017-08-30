7c73e0c46d0bf41ab7ec8bac23d034b1c379c5e0
Robert <robert.ghetau@pixelgrade.com>
2017-08-30 12:00:28 +0300

    user profile link

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@ $curauth = wp_get_current_user();
     <li><a href="//scores.dev/?page_id=46"><i class="fa fa-list-ol"></i>Leaderboard</a></li>
     <li><a href="#"><i class="fa fa-star"></i>Challenges</a></li>
     <li><a href="<?php echo get_post_type_archive_link( 'badges' ); ?>"><i class="fa fa-trophy"></i>Badges</a></li>
-    <li><a href="<?php echo get_author_posts_url($curauth->ID) ?>"><i class="fa fa-user"></i>Profile</a></li>
+    <li><a href="<?php echo get_author_posts_url(wp_get_current_user()->ID) ?>"><i class="fa fa-user"></i>Profile</a></li>
 </ul>
 
 </body>
