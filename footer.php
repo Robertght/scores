de5919083885c8b86fa2003d1e00d80738251d50
Razvan <razwan.me@gmail.com>
2017-08-30 18:02:55 +0300

    Remove challenges footer link

--- footer.php
+++ footer.php
@@ -18,7 +18,6 @@ $curauth = wp_get_current_user();
 <ul class="footer-nav">
     <li><a href="<?php echo get_page_link ( get_option( 'page_on_front' ) ); ?>"><i class="fa fa-home"></i>Home</a></li>
     <li><a href="<?php echo get_permalink( get_page_by_path( 'leaderboard' ) )?>"><i class="fa fa-list-ol"></i>Leaderboard</a></li>
-    <li><a href="#"><i class="fa fa-star"></i>Challenges</a></li>
     <li><a href="<?php echo get_post_type_archive_link( 'badges' ); ?>"><i class="fa fa-trophy"></i>Badges</a></li>
     <li><a href="<?php echo get_author_posts_url(wp_get_current_user()->ID) ?>"><i class="fa fa-user"></i>Profile</a></li>
 </ul>
