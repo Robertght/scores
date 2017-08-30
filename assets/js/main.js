bfc4be5b0468a76ef030a5dba0edf7025c969e02
Razvan <razwan.me@gmail.com>
2017-08-30 11:22:40 +0300

    improve styling for add match page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,42 @@
 (function( $, window, document, undefined ) {
 
 
-$(function() {
+$( function() {
 
 	$( '.js-back' ).click( function() {
 		window.history.back();
 	} );
 
-	$('.add-match-increase').click(function(e) {
+	$( '.add-match-increase' ).click( function( e ) {
 		e.preventDefault();
-		var $this = $(this),
-			$input = $this.closest('.add-match-team').find('.add-match-score'),
+
+		var $this = $( this ),
+			$input = $this.closest( '.add-match-team' ).find( '.add-match-score' ),
 			value = parseInt( $input.val(), 10 );
+
 		$input.val( value + 1 );
-	});
+	} );
 
-	$('.add-match-decrease').click(function(e) {
+	$( '.add-match-decrease' ).click( function( e ) {
 		e.preventDefault();
-		var $this = $(this),
-			$input = $this.closest('.add-match-team').find('.add-match-score'),
-			value = parseInt( $input.val(), 10 );
-		$input.val( value == 0 ? 0 : value - 1 );
-	});
 
-});
+		var $this = $( this ),
+			$input = $this.closest( '.add-match-team' ).find( '.add-match-score' ),
+			value = parseInt( $input.val(), 10 );
+
+		$input.val( value == 0 ? 0 : value - 1 );
+	} );
+
+	$( '.add-match-player' ).on( 'change', function() {
+		var $this = $( this ),
+			$selected = $this.find( ':selected' ),
+			avatar = $selected.data( 'avatar' ),
+			$container = $this.closest( '.add-match-team' ).find( '.add-match-avatar' ),
+			$img = $( '<img src="' + avatar + '">' );
+
+		$container.empty().append( $img );
+	} ).trigger( 'change' );
+
+} );
 
 })( jQuery, window, document );
--- page-add-match.php
+++ page-add-match.php
@@ -8,16 +8,21 @@ get_header();
     <form class="add-match" action="" id="primaryPostForm" method="POST">
 
         <div class="add-match-team add-match-home">
-            <label for="firstUserName" hidden><?php _e( 'First player:', 'scores' ) ?></label>
-            <select class="add-match-player" name="firstUserName" id="firstUserName">
-                <?php
-                $opponent_users = get_users();
-                foreach ( $opponent_users as $user ) { ?>
-                    <option value="<?php echo $user->ID; ?>" <?php if ( $user->ID == get_current_user_id() ) : echo 'selected="selected"'; endif; ?>><?php echo $user->data->display_name; ?></option>
-                <?php } ?>
-            </select>
-            <label for="firstUserScore" hidden><?php _e( 'First user score:', 'framework' ) ?></label>
-            <input class="add-match-score" type="number" name="firstUserScore" id="firstUserScore" value="0"/>
+            <div class="add-match-content">
+                <div class="add-match-avatar"></div>
+                <div class="add-match-info">
+                    <label for="firstUserName" hidden><?php _e( 'First player:', 'scores' ) ?></label>
+                    <select class="add-match-player" name="firstUserName" id="firstUserName">
+                        <?php
+                        $opponent_users = get_users();
+                        foreach ( $opponent_users as $user ) { ?>
+                            <option data-avatar="<?php echo get_avatar_url( $user->ID ); ?>" value="<?php echo $user->ID; ?>" <?php if ( $user->ID == get_current_user_id() ) : echo 'selected="selected"'; endif; ?>><?php echo $user->data->display_name; ?></option>
+                        <?php } ?>
+                    </select>
+                    <label for="firstUserScore" hidden><?php _e( 'First user score:', 'framework' ) ?></label>
+                    <input class="add-match-score" type="number" name="firstUserScore" id="firstUserScore" value="0"/>
+                </div>
+            </div>
             <div class="add-match-controls">
                 <button class="add-match-increase"><i class="fa fa-plus"></i></button>
                 <button class="add-match-decrease"><i class="fa fa-minus"></i></button>
@@ -25,21 +30,26 @@ get_header();
         </div>
 
         <div class="add-match-team add-match-away">
-            <label for="secondUserName" hidden><?php _e( 'Second player:', 'scores' ) ?></label>
-            <select class="add-match-player" name="secondUserName" id="secondUserName">
-                <?php
-                $args = array(
-                    'exclude' => array(
-                        get_current_user_id()
-                    ),
-                );
-                $opponent_users = get_users( $args );
-                foreach ( $opponent_users as $user ) { ?>
-                    <option value="<?php echo $user->ID; ?>"><?php echo $user->data->display_name; ?></option>
-                <?php } ?>
-            </select>
-            <label for="secondUserScore" hidden><?php _e( 'Second user score:', 'framework' ) ?></label>
-            <input class="add-match-score" type="number" name="secondUserScore" id="secondUserScore" value="0"/>
+            <div class="add-match-content">
+                <div class="add-match-avatar"></div>
+                <div class="add-match-info">
+                    <label for="secondUserName" hidden><?php _e( 'Second player:', 'scores' ) ?></label>
+                    <select class="add-match-player" name="secondUserName" id="secondUserName">
+                        <?php
+                        $args = array(
+                            'exclude' => array(
+                                get_current_user_id()
+                            ),
+                        );
+                        $opponent_users = get_users( $args );
+                        foreach ( $opponent_users as $user ) { ?>
+                            <option data-avatar="<?php echo get_avatar_url( $user->ID ); ?>" value="<?php echo $user->ID; ?>"><?php echo $user->data->display_name; ?></option>
+                        <?php } ?>
+                    </select>
+                    <label for="secondUserScore" hidden><?php _e( 'Second user score:', 'framework' ) ?></label>
+                    <input class="add-match-score" type="number" name="secondUserScore" id="secondUserScore" value="0"/>
+                </div>
+            </div>
             <div class="add-match-controls">
                 <button class="add-match-increase"><i class="fa fa-plus"></i></button>
                 <button class="add-match-decrease"><i class="fa fa-minus"></i></button>
