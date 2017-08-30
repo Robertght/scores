b4d818ca76009f566aa5e10b26786f95ab387fda
Robert <robert.ghetau@pixelgrade.com>
2017-08-30 10:57:33 +0300

    add leaderboard template&styling

--- leaderbord.php
+++ leaderbord.php
@@ -3,11 +3,21 @@
 <?php get_header(); ?>
 
 <?php
-$blogusers = get_users( array( 'fields' => array( 'display_name' ) ) );
+$blogusers = get_users();
 // Array of stdClass objects.
-foreach ( $blogusers as $user ) {
-    echo '<span>' . esc_html( $user->display_name ) . '</span>';
-}
+?><div class="leaderbord">
+    <div class="row-title"> Leaderboard </div>
+    <ul><?php foreach ( $blogusers as $user ) { ?>
+    <li><a href="<?php echo '/author/' . $user->data->user_login; ?>">
+            <div class="row-group">
+                <?php echo get_avatar( $user->ID ); ?>
+                <?php echo '<div class="user-name">' . esc_html( $user->display_name ) .'</div>'?> </div>
+            <div class="row-group">
+                <?php echo '<div class="row-score">' . 15 .'</div>'?>
+                <?php echo '<div class="row-xp">' . 1242 .'</div>'?>
+            </div>
+    </a></li>
+<?php } ?>
+</div></ul>
 
-?>
 <?php get_footer(); ?>
